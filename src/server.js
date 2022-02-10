16a852d17121e241adea0da823097a3d118e287e
xiwoo <xiwoo012@gmail.com>
2022-02-10 23:02:28 +0900

    불필요코드 삭제

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 
 import http from "http";
-// import WebSocket from "ws";
 import { Server } from "socket.io";
 import express from "express";
 
@@ -13,8 +12,6 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log("server!");
-
-// app.listen(3000, handleListen);
 
 const httpServer = http.createServer(app);
 const wsServer = new Server(httpServer);
@@ -40,39 +37,5 @@ wsServer.on("connection", (socket) => {
         done();
     })
 }); 
-
-// const wss = new WebSocket.Server({server});
-// const wss = new WebSocket.Server();//websocket만 사용할 때
-
-// function handleConnection(socket) {
-//     //socket:: 브라우저에서 넘어온 socket
-//     console.log(socket);
-// }
-// wss.on("connection", handleConnection);
-// const sockets = [];
-
-// wss.on("connection", (socket) => {
-//     sockets.push(socket);
-//     socket["nickname"] = "Anon"
-//     console.log("Connected to Browser ");
-//     socket.on("close", () => console.log("Disconnected from Browser X"))
-//     socket.on("message", (data) => {
-//         // console.log(data);
-//         const message = JSON.parse(data);
-//         switch(message.type) {
-//             case "new_message": 
-//                 sockets.forEach(sk => 
-//                     sk.send(`${socket.nickname}: ${message.payload}`)
-//                 );
-//                 break;
-//             case "nickname":
-//                 socket["nickname"] = message.payload;
-//                 break;
-//         }
-        
-//     });
-//     socket.send("hello!");
-// });
-
 
 httpServer.listen(3000, handleListen);
